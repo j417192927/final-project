f7726267dab64ae2e445f905105ebb845c2e8b9e
doejo <jasonli0620@gmail.com>
2016-04-27 10:06:37 -0500

    final project done

--- javascript.js
+++ javascript.js
@@ -2,18 +2,34 @@ jQuery(document).ready(function($) {
 $(".login-button").click(function(){
 	$(".login-index").fadeIn()
 });
-$(window).click(function(){
-	$("").fadeIn()
-});
+
 $(".login-button").click(function(){
 	$(".background-grey").fadeIn();
 	$("body").addClass("no-scroll");
 });
 
 $(".background-grey").click(function(){
-	$(".login-index").fadeOut()
-	$(".background-grey").fadeOut();
-	$("body").removeClass("no-scroll");
+  $(".background-grey").fadeOut();
+  $("body").removeClass("no-scroll");
+  $(".login-index").fadeOut()
+});
+
+
+
+
+var waypoint = new Waypoint({
+  element: $('.left-half-3'),
+  handler: function() {
+   $('.left-half-3').addClass("fadeInLeftBig");
+  },
+  offset: '25%'
+});
+var waypoint = new Waypoint({
+  element: $('.right-half-3'),
+  handler: function() {
+   $('.right-half-3').addClass("fadeInRightBig");
+  },
+  offset: '25%'
 });
 
 
@@ -21,7 +37,20 @@ $(".background-grey").click(function(){
 
 
 
-
+var waypoint = new Waypoint({
+  element: $('.small-pic'),
+  handler: function() {
+   $('.small-pic').addClass("fadeInLeftBig");
+  },
+  offset: '75%'
+});
+var waypoint = new Waypoint({
+  element: $('.small-pic-1'),
+  handler: function() {
+   $('.small-pic-1').addClass("fadeInRightBig");
+  },
+  offset: '75%'
+});
 
 
 
